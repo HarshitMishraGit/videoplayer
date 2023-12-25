f6274891788ae8ef91283d511eba9fee9ad15e32
Harshit Mishra <harshitlove28@outlook.com>
2023-12-25 14:02:40 +0530

    intiated storage and accessing it globally

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React,{useState} from 'react';
-import type {PropsWithChildren} from 'react';
+import React,{useState,useEffect} from 'react';
+import type { PropsWithChildren } from 'react';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -13,13 +14,22 @@ import {
   // Added Button component
 } from 'react-native';
 import DirectoryPickerComp from './components/DirectoryPickerComp';
-
-
+import storage from './storage/storage';
 function App(): React.JSX.Element {
-  const [fileResponse, setFileResponse] = useState([]);
+  const [dirName, setDirName] = useState("");
+  useEffect(() => {
+    console.log("DirName: ", dirName)
+    if (dirName && storage) {
+      storage.save({
+        key: 'dirName',  // Note: Do not use underscore("_") in key!
+        data: dirName,
+      });
+    }
+  }, [dirName,storage])
+  
   return (
     <SafeAreaView style={styles.container}>
-      <DirectoryPickerComp />
+      <DirectoryPickerComp  setDirName={setDirName}/>
       {/* <FlatList
         data={[{key: 'a'}, {key: 'b'}]}
         renderItem={({ item }) => <Text style={styles.text}>{item.key}</Text>}
--- components/DirectoryPickerComp.tsx
+++ components/DirectoryPickerComp.tsx
@@ -1,15 +1,27 @@
 import { View, Text ,StyleSheet, Button} from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DocumentPicker from 'react-native-document-picker';  
+import storage from '../storage/storage';
+export default function DirectoryPickerComp(props:any) {
+    const { setDirName } = props;
+    const [dir, setdir] = useState("")
+    let dirName = storage.load({
+        key: 'dirName',
+    }).then(res => {
+        console.log("res: ", res);
+        setdir(res);
+    }).catch(err => {
+        console.log("err: ", err);
+    })
 
-export default function DirectoryPickerComp() {
-
+    
     const handleDirectory = async () => {
         try {
           const res = await DocumentPicker.pickDirectory();
           console.log(
             res
-          );
+            );
+            setDirName(res?.uri);
         } catch (err) {
           if (DocumentPicker.isCancel(err)) {
             console.log('User cancelled the picker');
@@ -22,6 +34,7 @@ export default function DirectoryPickerComp() {
     <View>
           <Text style={styles.text}>DirectoryPickerComp</Text>
           <Button title="Click me" onPress={handleDirectory} />
+          <Text style={styles.text}>dir : { dir}</Text>
     </View>
   )
 }
